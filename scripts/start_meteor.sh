00fbf1d52ce2259b57d30fbd5d94eca0025af6a9
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-19 20:42:07 -0500

    Update script pour demarrer, utilise compte coupdoeil sur MongoDB

--- scripts/start_meteor.sh
+++ scripts/start_meteor.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Command pour creer compte MongoDB: 
+# use coupdoeil
+# db.createUser({user: "coupdoeil", pwd: "p1234", roles: [{ role: "readWrite", db: "coupdoeil" }]})
+
 PORT=[2607:f2c0:eb70:1785:215:5dff:fe01:1f00]:3000
-MONGO_URL='mongodb://root:example@192.168.1.110/'
+MONGO_URL='mongodb://meteor:p1234@192.168.1.110/coupdoeil'/'
 
 export MONGO_URL
 
