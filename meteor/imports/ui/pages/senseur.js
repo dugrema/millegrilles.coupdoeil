780ff6d6941c81f9333f58c8ab9c91066463b868
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-20 21:28:39 -0500

    Affichage des donnees horaires pour senseur

--- meteor/imports/ui/pages/senseur.js
+++ meteor/imports/ui/pages/senseur.js
@@ -24,3 +24,27 @@ Template.Senseur_show_page.helpers({
     return senseurdocs;
   },
 });
+
+Template.senseur_historique_horaire.helpers({
+  periode_short() {
+    var periode = this['periode'];
+    return periode.toISOString().split('T')[1].split('.')[0];
+  },
+  temperature_moyenne() {
+    return (Math.round(this['temperature-moyenne'] * 10) / 10).toFixed(1);
+  },
+  humidite_moyenne() {
+    var humidite = this['humidite-moyenne'];
+    if(humidite > 0) {
+      return Math.round(humidite);
+    }
+    return false;
+  },
+  pression_moyenne() {
+    var pression = this['pression-moyenne'];
+    if(pression > 0) {
+      return (Math.round(pression * 10) / 10).toFixed(1);
+    }
+    return false;
+  }
+});
