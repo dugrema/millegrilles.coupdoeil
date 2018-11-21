952cd467b6fe3a30b5ba7df018703b7f6a81aef8
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-21 16:18:06 -0500

    Changement version image docker

--- meteor/run-docker.sh
+++ meteor/run-docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source image_info.txt
+
 docker run -d \
   -e ROOT_URL=http://dev2.maple.mdugre.info \
   -e MONGO_URL=mongodb://coupdoeil:p1234@192.168.1.110/mg-sansnom \
   -p 3000:3000 \
-  registry.maple.mdugre.info:5000/coupdoeil.x86_64:v0.1.1
+  $REPO/$NAME.x86_64:$VERSION
