7b395aeb64606dc82364d6d6531c7666c3556de5
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-21 15:00:45 -0500

    Streamlined container Docker

--- meteor/run-docker.sh
+++ meteor/run-docker.sh
@@ -4,4 +4,4 @@ docker run -d \
   -e ROOT_URL=http://dev2.maple.mdugre.info \
   -e MONGO_URL=mongodb://coupdoeil:p1234@192.168.1.110/mg-sansnom \
   -p 3000:3000 \
-  coupdoeil
+  registry.maple.mdugre.info:5000/coupdoeil.x86_64:v0.1.1
