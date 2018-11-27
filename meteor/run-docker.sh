e1e0f9d4d7937f11b418a4b4a6fc300871f31ebe
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-27 13:27:40 -0500

    Ajustement oplog, test avec maple

--- meteor/run-docker.sh
+++ meteor/run-docker.sh
@@ -2,10 +2,15 @@
 
 source image_info.txt
 
+MG_MONGO_HOST=192.168.1.28
+MG_NOM_MILLEGRILLE=maple
+MG_MONGO_USER=coupdoeilmeteor
+MG_MONGO_PASSWORD=MONGO_PWD
+
 docker run -d \
   -e ROOT_URL=http://dev2.maple.mdugre.info \
-  -e MONGO_URL=mongodb://coupdoeil:p1234@192.168.1.110/mg-sansnom?ssl=true \
-  -e MONGO_OPLOG_URL=mongodb://root:example@192.168.1.110/local?ssl=true\&authSource=admin \
+  -e MONGO_URL=mongodb://$MG_MONGO_USER:$MG_MONGO_PASSWORD@$MG_MONGO_HOST/mg-$MG_NOM_MILLEGRILLE?ssl=true \
+  -e MONGO_OPLOG_URL=mongodb://$MG_MONGO_USER:$MG_MONGO_PASSWORD@$MG_MONGO_HOST/local?ssl=true\&authSource=admin \
   -p 3000:3000 \
   $REPO/$NAME.x86_64:$VERSION
 
