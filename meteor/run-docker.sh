4646212c114d9b3696bf4b2070768986ecb7edc5
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-11-27 13:10:44 -0500

    Ajout url oplog

--- meteor/run-docker.sh
+++ meteor/run-docker.sh
@@ -4,6 +4,9 @@ source image_info.txt
 
 docker run -d \
   -e ROOT_URL=http://dev2.maple.mdugre.info \
-  -e MONGO_URL=mongodb://coupdoeil:p1234@192.168.1.110/mg-sansnom \
+  -e MONGO_URL=mongodb://coupdoeil:p1234@192.168.1.110/mg-sansnom?ssl=true \
+  -e MONGO_OPLOG_URL=mongodb://root:example@192.168.1.110/local?ssl=true\&authSource=admin \
   -p 3000:3000 \
   $REPO/$NAME.x86_64:$VERSION
+
+
