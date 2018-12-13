ec94e072184f4bb27258ba1a580514f93f12e485
Mathieu Dugre <mathieu.dugre@mdugre.info>
2018-12-12 19:40:40 -0500

    Ajout list de notifications pour SenseursPassifs.

--- meteor/imports/ui/pages/notifications.js
+++ meteor/imports/ui/pages/notifications.js
@@ -4,11 +4,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Template } from 'meteor/templating';
 
 import { Notifications } from '../../api/millegrilles_domaines_Notifications.js';
+import { SenseursPassifs } from '../../api/mgdomaines_appareils_SenseursPassifs.js';
 
 import './notifications.html'
 
 Template.Notifications_show_page.onCreated(function bodyOnCreated() {
   Meteor.subscribe('notifications_regles_simples');
+  Meteor.subscribe('senseurs');
 });
 
 Template.Notification_liste.helpers({
@@ -17,5 +19,15 @@ Template.Notification_liste.helpers({
   },
 });
 
-Template.Notification_ligne.helpers({
-});
+Template.mgdomaines_appareils_SenseursPassifs.helpers({
+  charger_senseur(source) {
+    // return [{"senseur": "Ah!", "source_id": source._id}];
+    let document_senseur = SenseursPassifs.findOne({
+      '_id': new Meteor.Collection.ObjectID(source._id)
+    });
+    return document_senseur;
+  },
+  nom_variable() {
+    return 'mgdomaines_appareils_SenseursPassifs_' + this.valeurs.element;
+  }
+})
