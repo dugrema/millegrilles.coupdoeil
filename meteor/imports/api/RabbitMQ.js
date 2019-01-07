0af7edd51fcba0b90a6ceba8ecaddabdc8f05b46
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-01-06 21:55:10 -0500

    Ajout parametres certs pour RabbitMQ.

--- meteor/imports/api/RabbitMQ.js
+++ meteor/imports/api/RabbitMQ.js
@@ -24,13 +24,23 @@ class RabbitMQWrapper {
   _connect() {
     var fs = require('fs');
 
-    let mq_cert = process.env.MG_MQ_CERT;
+    let mq_cacert = process.env.MG_MQ_CAFILE,
+        mq_cert = process.env.MG_MQ_CERTFILE,
+        mq_key = process.env.MG_MQ_KEYFILE;
 
     if(this.connection === null) {
       let options = {}
+      if(mq_cacert !== undefined) {
+        var cacert = fs.readFileSync(mq_cacert);
+        options['ca'] = [cacert];
+      }
       if(mq_cert !== undefined) {
-        var sscert = fs.readFileSync(mq_cert);
-        options['ca'] = [sscert];
+        var cert = fs.readFileSync(mq_cert);
+        options['cert'] = cert;
+      }
+      if(mq_key !== undefined) {
+        var key = fs.readFileSync(mq_key);
+        options['key'] = key;
       }
 
       var open = this.amqp.connect(this.params, options);
--- scripts/start_meteor.sh
+++ scripts/start_meteor.sh
@@ -6,17 +6,26 @@
 
 #PORT=[2607:f2c0:eb70:1785:215:5dff:fe01:1f00]:3000
 PORT=192.168.2.103:3000
-MONGO_URL='mongodb://coupdoeilmeteor:p1234@192.168.2.6/mg-sansnom?ssl=true'
-MONGO_OPLOG_URL='mongodb://coupdoeilmeteor:p1234@192.168.2.6/local?ssl=true&authSource=mg-sansnom'
+MONGO_URL='mongodb://coupdoeilmeteor:p1234@mongo/mg-sansnom?ssl=true'
+MONGO_OPLOG_URL='mongodb://coupdoeilmeteor:p1234@mongo/local?ssl=true&authSource=mg-sansnom'
 MG_NOM_MILLEGRILLE=sansnom
-MG_MQ_HOST=192.168.2.6
+MG_MQ_HOST=mq
 MG_MQ_PORT=5673
 MG_MQ_USER=dev
 MG_MQ_PASSWORD=dev_user1
 #MG_MQ_CERT='/home/mathieu/git/MilleGrilles.coupdoeil/cert/dev2.cer'
-MG_MQ_PROTOCOL='amqp'
+#MG_MQ_PROTOCOL='amqp'
 
-export MONGO_URL MONGO_OPLOG_URL MG_MQ_HOST MG_MQ_PORT MG_MQ_USER MG_MQ_PASSWORD MG_NOM_MILLEGRILLE MG_MQ_PROTOCOL
+MG_MQ_CAFILE=/usr/local/etc/millegrilles/certs/millegrilles.authority.pem
+MG_MQ_CERTFILE=/usr/local/etc/millegrilles/certs/dev1.cert.pem
+MG_MQ_KEYFILE=/home/mathieu/certificates/millegrilles/privkeys/dev1.pem
+export MG_MQ_CAFILE MG_MQ_CERTFILE MG_MQ_KEYFILE
+
+# export MONGO_URL MONGO_OPLOG_URL MG_MQ_HOST MG_MQ_PORT MG_MQ_USER MG_MQ_PASSWORD MG_NOM_MILLEGRILLE MG_MQ_PROTOCOL
+export MG_MQ_HOST MG_MQ_PORT MG_MQ_USER MG_MQ_PASSWORD MG_NOM_MILLEGRILLE MG_MQ_PROTOCOL
 #export MG_MQ_CERT
 
+# METEOR_SETTINGS=/home/mathieu/git/MilleGrilles.coupdoeil/meteor/dev_settings.json
+
 meteor --port $PORT
+# --settings $METEOR_SETTINGS
