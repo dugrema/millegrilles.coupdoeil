cf5974100df936842c47a7ba7e0038d078acc7f1
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-06 14:38:26 -0400

    Update nginx pour dev avec node.js local.

--- dev/nginx/run.sh
+++ dev/nginx/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -p 8443:443 -p 8080:80 -v /home/mathieu/git/MilleGrilles.coupdoeil/dev/nginx/conf.d/:/etc/nginx/conf.d nginx:mg_dev
+docker run -d -p 8443:443 -p 8080:80 -v /home/mathieu/git/MilleGrilles.coupdoeil/dev/nginx/conf.d/:/etc/nginx/conf.d nginx:mg_dev
