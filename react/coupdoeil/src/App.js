1f13eda5ea29906616e6c18f36e761726777beda
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-07 17:14:17 -0400

    Ajout router, page auth en exemple.

--- react/coupdoeil/src/App.js
+++ react/coupdoeil/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Authentification from './authentification'
 
-function App() {
+function Index() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +25,26 @@ function App() {
   );
 }
 
-export default App;
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/authentification/">Authentification</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/authentification/" component={Authentification} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
---
+++ react/coupdoeil/src/authentification.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Authentification() {
+  return (
+    <div className="Authentification">
+      <header className="App-header">
+        <p>
+          Authentification avec token USB.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Authentification;
