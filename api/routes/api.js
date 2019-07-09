69c3d2fe0c96cb5bff7530bf0d6c8fde3b8c8eff
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-09 18:25:01 -0400

    Creation singleton pour RabbitMQ connexion.

--- api/routes/api.js
+++ api/routes/api.js
@@ -5,6 +5,8 @@ var amqp = require('amqplib');
 var fs = require('fs');
 
 console.log(rabbitMQ);
+let mqConnectionUrl = 'amqps://dev2:dev2@dev2:5673/dev2';
+rabbitMQ.singleton.connect(mqConnectionUrl);
 
 /* Sample. */
 router.get('/', function(req, res, next) {
@@ -22,18 +24,18 @@ router.post('/requete', function(req, res, next) {
   console.log("POST /requete: ")
   console.log(req.body);
 
-  const options = {};
-  var cacert = fs.readFileSync('/home/mathieu/certificates/millegrilles/pki.millegrilles.ssl.CAchain');
-  options['ca'] = [cacert];
-  var cert = fs.readFileSync('/home/mathieu/certificates/millegrilles/certs/dev2.maple.mdugre.info.cert.pem');
-  options['cert'] = cert;
-  var key = fs.readFileSync('/home/mathieu/certificates/millegrilles/privkeys/dev2.maple.mdugre.info.pem');
-  options['key'] = key;
+  // const options = {};
+  // var cacert = fs.readFileSync('/home/mathieu/certificates/millegrilles/pki.millegrilles.ssl.CAchain');
+  // options['ca'] = [cacert];
+  // var cert = fs.readFileSync('/home/mathieu/certificates/millegrilles/certs/dev2.maple.mdugre.info.cert.pem');
+  // options['cert'] = cert;
+  // var key = fs.readFileSync('/home/mathieu/certificates/millegrilles/privkeys/dev2.maple.mdugre.info.pem');
+  // options['key'] = key;
+  //
+  // var open = amqp.connect(mqConnectionUrl, options);
+  // console.log("OK!");
 
-  let mqConnectionUrl = 'amqps://dev2:dev2@dev2:5673/dev2';
-
-  var open = amqp.connect(mqConnectionUrl, options);
-  console.log("OK!");
+  rabbitMQ.singleton.transmettreTransactionFormattee({'test': 'valeur'}, 'test');
 
   // Attendre la reponse - callback apres 1 seconde si erreur (timeout)
   reponse = {
@@ -42,48 +44,50 @@ router.post('/requete', function(req, res, next) {
     'requete': req.body
   };
 
-  var repondu = false;
-  reponse_fonction = function() {
-    if(!repondu) {
-      repondu = true;
-      res.json(reponse);
-    } else {
-      console.log("Reponse redondante");
-    }
-  }
-  setTimeout(reponse_fonction, 1000);
+  res.json(reponse);
 
-  let conn, channel, ch;
-  open
-    .then( conn_in => {
-      console.log("Connexion reussie, ouverture channel");
-      conn = conn_in;
-      conn.on("error", function(err) {
-        console.error("Erreur connexion MQ");
-        console.error(err);
-      });
-      channel = conn.createChannel();
-      channel
-        .then( (ch) => {
-          channel = ch;
-          reponse['OK'] = "L'affaire est poutine!";
-          console.log("Channel ouvert");
-          console.log(ch);
+  // var repondu = false;
+  // reponse_fonction = function() {
+  //   if(!repondu) {
+  //     repondu = true;
+  //     res.json(reponse);
+  //   } else {
+  //     console.log("Reponse redondante");
+  //   }
+  // }
+  // setTimeout(reponse_fonction, 1000);
 
-          // Transmettre reponse
-          reponse_fonction();
-
-        })
-        .catch( (err) => {
-          console.log("Erreur ouverture channel!");
-          console.error(err);
-        })
-
-    })
-    .catch((err) => {
-      console.log("Erreur!");
-      console.error(err);
-    });
+  // let conn, channel, ch;
+  // open
+  //   .then( conn_in => {
+  //     console.log("Connexion reussie, ouverture channel");
+  //     conn = conn_in;
+  //     conn.on("error", function(err) {
+  //       console.error("Erreur connexion MQ");
+  //       console.error(err);
+  //     });
+  //     channel = conn.createChannel();
+  //     channel
+  //       .then( (ch) => {
+  //         channel = ch;
+  //         reponse['OK'] = "L'affaire est poutine!";
+  //         console.log("Channel ouvert");
+  //         console.log(ch);
+  //
+  //         // Transmettre reponse
+  //         reponse_fonction();
+  //
+  //       })
+  //       .catch( (err) => {
+  //         console.log("Erreur ouverture channel!");
+  //         console.error(err);
+  //       })
+  //
+  //   })
+  //   .catch((err) => {
+  //     console.log("Erreur!");
+  //     console.error(err);
+  //   });
 
 });
 
--- api/routes/res/rabbitMQ.js
+++ api/routes/res/rabbitMQ.js
@@ -3,7 +3,7 @@ var Amqplib = require('amqplib');
 class RabbitMQWrapper {
 
   constructor() {
-    this.params = null;
+    this.url = null;
     this.connection = null;
     this.channel = null;
 
@@ -16,8 +16,8 @@ class RabbitMQWrapper {
     // this.setHostname();
   }
 
-  connect(connection) {
-    this.params = connection;
+  connect(url) {
+    this.url = url;
     this._connect();
   }
 
@@ -30,31 +30,99 @@ class RabbitMQWrapper {
 
     if(this.connection === null) {
       let options = {}
-      // if(mq_cacert !== undefined) {
-      //   var cacert = fs.readFileSync(mq_cacert);
-      //   options['ca'] = [cacert];
-      // }
-      // if(mq_cert !== undefined) {
-      //   var cert = fs.readFileSync(mq_cert);
-      //   options['cert'] = cert;
-      // }
-      // if(mq_key !== undefined) {
-      //   var key = fs.readFileSync(mq_key);
-      //   options['key'] = key;
-      // }
+      if(mq_cacert !== undefined) {
+        var cacert = fs.readFileSync(mq_cacert);
+        options['ca'] = [cacert];
+      }
+      if(mq_cert !== undefined) {
+        var cert = fs.readFileSync(mq_cert);
+        options['cert'] = cert;
+      }
+      if(mq_key !== undefined) {
+        var key = fs.readFileSync(mq_key);
+        options['key'] = key;
+      }
 
-      var open = this.amqp.connect(this.params, options);
-      // open.then(function (conn) {
-      //   RabbitMQ.setConnection(conn);
-      //   RabbitMQ.ouvrirChannel();
-      // });
+      var open = Amqplib.connect(this.url, options);
+      open
+        .then( conn => {
+          console.debug("Connexion a RabbitMQ reussie");
+          this.connection = conn;
+          this.ouvrirChannel();
+        })
+        .catch(err => {
+          this.connection = null;
+          console.error("Erreur connexion RabbitMQ");
+          console.error(err);
+        });
 
     }
 
   }
 
+  ouvrirChannel() {
+    this.connection.createChannel()
+      .then( (ch) => {
+        this.channel = ch;
+        console.log("Channel ouvert");
+        console.log(ch);
+      })
+      .catch( (err) => {
+        this.channel = null;
+        console.log("Erreur ouverture channel!");
+        console.error(err);
+      })
+  }
+
+  // Utiliser cette methode pour simplifier le formattage d'une transaction.
+  // Il faut fournir le contenu de la transaction et le domaine (routing)
+  transmettreTransactionFormattee(message, domaine) {
+    let messageFormatte = message;  // Meme objet si ca cause pas de problemes
+
+    //let infoTransaction = this._formatterInfoTransaction(domaine);
+    let infoTransaction = {'entete': 'information inutile!'};
+    messageFormatte['en-tete'] = infoTransaction;
+
+    // Signer le message
+    // this._signerMessage(messageFormatte);
+
+    let routingKey = 'transaction.nouvelle';
+    this._transmettreTransaction(routingKey, messageFormatte);
+  }
+
+  // Methode qui permet de transmettre une transaction au backend RabbitMQ
+  // Les metadonnees sont ajoutees automatiquement
+  _transmettreTransaction(routingKey, message) {
+    let jsonMessage = JSON.stringify(message);
+
+    // Le code doit uniquement etre execute sur le serveur
+    console.log("Message: routing=" + routingKey + " message=" + jsonMessage);
+    try {
+      console.log("Message a transmettre: " + routingKey + " = " + jsonMessage);
+      this.channel.publish(
+        'millegrilles.noeuds',
+        routingKey,
+         new Buffer(jsonMessage),
+         {},
+         function(err, ok) {
+           console.error("Erreur MQ Callback");
+           console.error(err);
+         }
+      );
+    }
+    catch (e) {
+      console.error("Erreur MQ");
+      console.error(e);
+      this.reconnect(); // Tenter de se reconnecter
+    }
+  }
+
+
 }
 
-var rabbitMQ = RabbitMQWrapper;
+const rabbitMQ_singleton = new RabbitMQWrapper();
 
-module.exports = rabbitMQ;
+module.exports = {
+  'RabbitMQWrapper': RabbitMQWrapper,
+  'singleton': rabbitMQ_singleton
+};
