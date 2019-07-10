df8396165ee8cbd6cd0488f3fb3631bba0837502
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-10 09:10:30 -0400

    Ajout script start avec variables env.

---
+++ api/start_dev2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CERT_FOLDER=/home/mathieu/certificates
+
+export MG_MQ_CAFILE=$CERT_FOLDER/millegrilles/pki.millegrilles.ssl.CAchain
+export MG_MQ_CERTFILE=$CERT_FOLDER/millegrilles/certs/dev2.maple.mdugre.info.cert.pem
+export MG_MQ_KEYFILE=$CERT_FOLDER/millegrilles/privkeys/dev2.maple.mdugre.info.pem
+export CERT=$CERT_FOLDER/dev2.maple.mdugre.info/fullchain.pem
+export PRIVKEY=$CERT_FOLDER/dev2.maple.mdugre.info/privkey.pem
+export MG_MQ_URL=amqps://dev2:5673/dev2
+export PORT=3001
+
+npm start
