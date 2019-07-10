91211052870c594263ee5475c3e647e748bfed73
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-09 21:08:36 -0400

    Ajout reponse via correlation, promise callback.

--- api/routes/api.js
+++ api/routes/api.js
@@ -4,7 +4,6 @@ var router = express.Router();
 var amqp = require('amqplib');
 var fs = require('fs');
 
-console.log(rabbitMQ);
 let mqConnectionUrl = 'amqps://dev2:dev2@dev2:5673/dev2';
 rabbitMQ.singleton.connect(mqConnectionUrl);
 
@@ -24,19 +23,6 @@ router.post('/requete', function(req, res, next) {
   console.log("POST /requete: ")
   console.log(req.body);
 
-  // const options = {};
-  // var cacert = fs.readFileSync('/home/mathieu/certificates/millegrilles/pki.millegrilles.ssl.CAchain');
-  // options['ca'] = [cacert];
-  // var cert = fs.readFileSync('/home/mathieu/certificates/millegrilles/certs/dev2.maple.mdugre.info.cert.pem');
-  // options['cert'] = cert;
-  // var key = fs.readFileSync('/home/mathieu/certificates/millegrilles/privkeys/dev2.maple.mdugre.info.pem');
-  // options['key'] = key;
-  //
-  // var open = amqp.connect(mqConnectionUrl, options);
-  // console.log("OK!");
-
-  rabbitMQ.singleton.transmettreTransactionFormattee({'requete': req.body});
-
   // Attendre la reponse - callback apres 1 seconde si erreur (timeout)
   reponse = {
     'doc_json': true,
@@ -44,50 +30,34 @@ router.post('/requete', function(req, res, next) {
     'requete': req.body
   };
 
-  res.json(reponse);
+  var repondu = false;
+  reponse_fonction = function() {
+    if(!repondu) {
+      repondu = true;
+      res.json(reponse);
+    } else {
+      console.log("Reponse redondante");
+    }
+  }
+  setTimeout(reponse_fonction, 1000);
 
-  // var repondu = false;
-  // reponse_fonction = function() {
-  //   if(!repondu) {
-  //     repondu = true;
-  //     res.json(reponse);
-  //   } else {
-  //     console.log("Reponse redondante");
-  //   }
-  // }
-  // setTimeout(reponse_fonction, 1000);
+  reponse_serveur = rabbitMQ.singleton.transmettreRequete(
+    'test.routing',
+    {'requete': req.body}
+  )
+  .then((msg) => {
+    console.log("Message on the way back!");
+    let correlationId = msg.properties.correlationId;
+    let messageContent = decodeURIComponent(escape(msg.content));
+    let routingKey = msg.fields.routingKey;
 
-  // let conn, channel, ch;
-  // open
-  //   .then( conn_in => {
-  //     console.log("Connexion reussie, ouverture channel");
-  //     conn = conn_in;
-  //     conn.on("error", function(err) {
-  //       console.error("Erreur connexion MQ");
-  //       console.error(err);
-  //     });
-  //     channel = conn.createChannel();
-  //     channel
-  //       .then( (ch) => {
-  //         channel = ch;
-  //         reponse['OK'] = "L'affaire est poutine!";
-  //         console.log("Channel ouvert");
-  //         console.log(ch);
-  //
-  //         // Transmettre reponse
-  //         reponse_fonction();
-  //
-  //       })
-  //       .catch( (err) => {
-  //         console.log("Erreur ouverture channel!");
-  //         console.error(err);
-  //       })
-  //
-  //   })
-  //   .catch((err) => {
-  //     console.log("Erreur!");
-  //     console.error(err);
-  //   });
+    reponse['correlationId'] = correlationId;
+    reponse_fonction();
+  })
+  .catch(err => {
+    console.error("Erreur!");
+    console.error(err);
+  });
 
 });
 
--- api/routes/res/rabbitMQ.js
+++ api/routes/res/rabbitMQ.js
@@ -1,4 +1,7 @@
-var Amqplib = require('amqplib');
+var amqplib = require('amqplib');
+var os = require('os');
+var fs = require('fs');
+uuidv4 = require('uuid/v4');
 
 class RabbitMQWrapper {
 
@@ -9,8 +12,10 @@ class RabbitMQWrapper {
 
     this.reconnectTimeout = null; // Timer de reconnexion - null si inactif
 
-    // this.uuidv4 = require('uuid/v4');
-    // this.os = require('os');
+    // Correlation avec les reponses en attente.
+    // Cle: uuid de CorrelationId
+    // Valeur: callback
+    this.pendingResponses = {};
 
     // this.nomMilleGrille = this._trouverNomMilleGrille()
     // this.setHostname();
@@ -22,7 +27,6 @@ class RabbitMQWrapper {
   }
 
   _connect() {
-    var fs = require('fs');
 
     let mq_cacert = process.env.MG_MQ_CAFILE,
         mq_cert = process.env.MG_MQ_CERTFILE,
@@ -43,50 +47,75 @@ class RabbitMQWrapper {
         options['key'] = key;
       }
 
-      Amqplib.connect(this.url, options)
+      amqplib.connect(this.url, options)
         .then( conn => {
           console.debug("Connexion a RabbitMQ reussie");
           this.connection = conn;
-          this.ouvrirChannel();
+
+          conn.createChannel()
+            .then( (ch) => {
+              this.channel = ch;
+              console.log("Channel ouvert");
+              this.ecouter();
+            })
+            .catch(err => {
+              console.error("Erreur ouverture channel");
+              console.error(err);
+            })
+
         })
         .catch(err => {
           this.connection = null;
           console.error("Erreur connexion RabbitMQ");
           console.error(err);
+        })
+        .then( () => {
+          console.log("Connexion et channel prets!");
         });
 
     }
 
   }
 
-  ouvrirChannel() {
-    this.connection.createChannel()
-      .then( (ch) => {
-        this.channel = ch;
-        console.log("Channel ouvert");
-        console.log(ch);
-
-        this.ecouter();
-      })
-      .catch( (err) => {
-        this.channel = null;
-        console.log("Erreur ouverture channel!");
-        console.error(err);
-      })
-  }
-
   ecouter() {
-    this.channel.consume(
-      'erreurs_processus',
-      (msg) => {
-        console.log("Message: ");
-        console.log(msg);
 
-        console.log("Routing key: " + msg.fields.routingKey);
-        console.log("Message: " + decodeURIComponent(escape(msg.content)));
-      },
-      {noAck: true}
-    );
+    // Creer Q pour ecouter
+    this.channel.assertQueue('', {
+      durable: false,
+      exclusive: true,
+    })
+    .then( (q) => {
+      console.log("Queue cree"),
+      console.log(q);
+
+      this.channel.bindQueue(q.queue, 'millegrilles.noeuds', 'test.routing');
+
+      this.channel.consume(
+        q.queue,
+        (msg) => {
+          let correlationId = msg.properties.correlationId;
+          let messageContent = decodeURIComponent(escape(msg.content));
+          let routingKey = msg.fields.routingKey;
+          // console.log("Message: ");
+          // console.log(msg);
+          console.log("Routing key: " + routingKey);
+          console.log("Message: " + messageContent);
+          console.log("CorrelationId: " + correlationId);
+
+          let callback = this.pendingResponses[correlationId];
+          if(callback) {
+            callback(msg);
+          }
+        },
+        {noAck: true}
+      );
+
+    })
+    .catch( err => {
+      console.error("Erreur creation Q pour ecouter");
+      console.error(err);
+    })
+
   }
 
   // Utiliser cette methode pour simplifier le formattage d'une transaction.
@@ -132,6 +161,52 @@ class RabbitMQWrapper {
     }
   }
 
+  transmettreRequete(routingKey, message) {
+
+    let correlation = uuidv4();
+    message['correlation'] = correlation;
+    let jsonMessage = JSON.stringify(message);
+    console.log("Message a transmettre: " + routingKey + " = " + jsonMessage);
+
+    let promise = new Promise((resolve, reject) => {
+
+      var processed = false;
+      const pendingResponses = this.pendingResponses;
+      pendingResponses[correlation] = function(msg, err) {
+        processed = true;
+        // Cleanup du callback
+        delete pendingResponses[correlation];
+        // console.log("Message recu dans callback");
+        // console.log(msg);
+
+        if(!err) {
+          resolve(msg);
+        } else {
+          reject(err);
+        }
+      };
+
+      // Faire la publication
+      this.channel.publish(
+        'millegrilles.noeuds',
+        routingKey,
+        new Buffer(jsonMessage),
+        {
+          correlationId: message['correlation']
+        },
+        function(err, ok) {
+          console.error("Erreur MQ Callback");
+          console.error(err);
+          delete this.pendingResponses[correlation];
+        }
+      );
+
+    });
+
+    // Attendre la reponse a la Requete
+
+    return promise;
+  }
 
 }
 
