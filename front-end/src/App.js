ca63ad77a7f78075d8e28fd28e77d9f8ebdc3b78
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-14 15:43:40 -0400

    Socket transfere dans state de l'application.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,37 +13,6 @@ const fakeAuth = {
   key: null,
   id: 'a1ID',
   email: 'test@test.com',
-  async authenticate(cb) {
-    const challenge = await fetch(urlApi + '/api/login', {
-        method: 'POST',
-        headers: {
-            'content-type': 'Application/Json'
-        },
-        body: JSON.stringify({ email: 'test@test' })
-    })
-    .then(response => response.json());
-
-    const credentials = await solveLoginChallenge(challenge);
-    const { loggedIn } = await fetch(
-        urlApi + '/api/login-challenge',
-        {
-            method: 'POST',
-            headers: {
-                'content-type': 'Application/Json'
-            },
-            body: JSON.stringify(credentials)
-        }
-    ).then(response => response.json());
-
-    if (loggedIn) {
-      this.isAuthenticated = true;
-      this.challenge = challenge.challenge;
-      console.debug('You are logged in');
-    } else {
-      console.error('Invalid credential');
-    }
-    cb(this.isAuthenticated); // Callback
-  },
   async register(cb) {
 
     const challenge = await fetch(urlApi + '/api/initialiser-empreinte', {
@@ -79,66 +48,19 @@ const fakeAuth = {
     }
 
     cb(); // Callback
-  },
-  signout(cb) {
-    this.isAuthenticated = false;
-    setTimeout(cb, 100);
   }
 };
 
 class Login extends React.Component {
   state = {
-    redirectToReferrer: false,
-    loggedIn: false
+    redirectToReferrer: false
   };
 
   register = () => {
     fakeAuth.register(() => {
       this.setState({ redirectToReferrer: true });
-      console.log("Callback register complete");
+      // console.log("Callback register complete");
     });
-  };
-
-  /* login = () => {
-    console.log("Login called: 1")
-    fakeAuth.authenticate(() => {
-      //this.setState({loggedIn: true});
-      this.props.setLoginState(true);
-      console.log("Callback authenticate complete");
-    });
-  };*/
-
-  checkStatus = () => {
-    let socket = openSocket('/');
-    socket.on('allo', reponse => {
-      console.log("Allo!");
-      console.log(reponse);
-    });
-
-    socket.on('disconnect', () => {
-      console.log("Disconnected");
-    });
-
-    /*
-    fetch(urlApi + '/api/requete', {
-        method: 'POST',
-        headers: {
-            'content-type': 'Application/Json',
-            'auth-token': fakeAuth.challenge
-        },
-        body: JSON.stringify({
-          "requetes": [
-            {
-              "filtre": {
-                "_mg-libelle": "profil.usager"
-              }
-            }
-          ]
-        })
-    }).then(response => {
-      console.log("Status login: " + response);
-    });
-    */
   };
 
   render() {
@@ -150,8 +72,6 @@ class Login extends React.Component {
         <button onClick={this.props.login_method}>Log in</button>
         <p>Register now!</p>
         <button onClick={this.register}>Register</button>
-        <p>Check status</p>
-        <button onClick={this.checkStatus}>Check</button>
       </div>
     );
   }
@@ -159,15 +79,16 @@ class Login extends React.Component {
 
 class App extends React.Component {
   state = {
-    loggedIn: false
+    loggedIn: false,
+    wss_socket: null
   }
 
+  // Web Socket pour communiquer avec le serveur
   wss_socket;
 
   login = () => {
-    console.log("Login called: 3")
-
-    if(!this.wss_socket) {
+    if(!this.state.wss_socket) {
+      // Ouvrir un nouveau socket vers le serveur
       let socket = openSocket('/');
 
       // Enregistrer evenements generiques
@@ -176,41 +97,35 @@ class App extends React.Component {
         console.error(erreur);
       })
       socket.on('disconnect', () => {
-        console.log("Disconnected");
-        this.wss_socket = null;
-        this.setState({loggedIn: false});
+        console.warn("Disconnected");
+        this.setState({wss_socket: null, loggedIn: false});
       });
 
-      // Gerer l'authentification
+      // Gerer l'authentification:
+      // 1. Ecouter le challenge du serveur
       socket.on('challenge', challenge => {
-        console.debug("Challenge recu");
-        console.debug(challenge);
 
         socket.on('login', confirmation=>{
-          this.setState({loggedIn: true});
-          console.log("Callback authenticate complete");
-          this.wss_socket = socket;
+          this.setState({loggedIn: confirmation, wss_socket: socket});
         });
 
-        console.debug("On va solver!");
-
+        // 2. Resoudre challenge et repondre au serveur pour activer connexion
         solveLoginChallenge(challenge)
         .then(credentials => {
-          console.debug("Tranmission evenement challenge_reply");
+          // console.debug("Tranmission evenement challenge_reply");
           socket.emit('challenge_reply', credentials);
         })
         .catch(err=>{
           console.error("Erreur challenge reply");
           console.error(err);
+          try{this.state.wss_socket.disconnect();} catch(err) {}
+          this.setState({wss_socket: null});
         });
 
       });
 
-
-      // socket.emit('authentification', '');
-
     } else {
-      throw "Login: WebSocket deja ouvert";
+      throw {erreur: "Login: WebSocket deja ouvert"};
     }
   };
 
