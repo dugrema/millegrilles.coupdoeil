570f459aa13cca872aa733992a091b1ef0f98da0
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-19 21:33:38 -0400

    Cleanup routes pas utilisees.

--- api/app.js
+++ api/app.js
@@ -3,53 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var multer = require('multer');
 
+// Routes
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var grosFichiersRouter = require('./routes/grosFichiers');
-var sessionManagement = require('./routes/res/sessionManagement.js');
 
 var app = express();
 
-var stagingFolder = process.env.MG_STAGING_FOLDER || "/tmp/uploadStaging";
-
-var multer_fn = multer({dest: stagingFolder}).array('grosfichier');
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-function authentication(req, res, next) {
-  let url = req.url;
-
-  if(url.startsWith('/grosFichiers')) {
-    // Pour le transfert de fichiers, il faut fournir un token de connexion
-    let authtoken = req.headers.authtoken;
-    // console.debug("AUTH: Recu token " + authtoken);
-    if(sessionManagement.consommerToken(authtoken)) {
-      // console.debug("Token consomme: " + authtoken + ", accepte.");
-      next();
-    } else {
-      // console.error("Token invalide pour transfert fichier" + authToken);
-      res.sendStatus(403);
-    }
-  } else {
-    // URL n'est pas protege
-    next();
-  }
-
-  return;
-};
-
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(authentication);  // Utilise pour transfert de fichiers
-app.use(multer_fn);
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
 app.use('/grosFichiers', grosFichiersRouter);
 
--- api/routes/grosFichiers.js
+++ api/routes/grosFichiers.js
@@ -2,6 +2,35 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var sessionManagement = require('./res/sessionManagement');
+var multer = require('multer');
+
+var stagingFolder = process.env.MG_STAGING_FOLDER || "/tmp/uploadStaging";
+var multer_fn = multer({dest: stagingFolder}).array('grosfichier');
+
+function authentication(req, res, next) {
+  let url = req.url;
+
+  if(url.startsWith('/grosFichiers')) {
+    // Pour le transfert de fichiers, il faut fournir un token de connexion
+    let authtoken = req.headers.authtoken;
+    // console.debug("AUTH: Recu token " + authtoken);
+    if(sessionManagement.consommerToken(authtoken)) {
+      // console.debug("Token consomme: " + authtoken + ", accepte.");
+      next();
+    } else {
+      // console.error("Token invalide pour transfert fichier" + authToken);
+      res.sendStatus(403);
+    }
+  } else {
+    // URL n'est pas protege
+    next();
+  }
+
+  return;
+};
+
+router.use(authentication);  // Utilise pour transfert de fichiers
+router.use(multer_fn);
 
 router.put('/nouveauFichier', function(req, res, next) {
   // console.log('Fichiers recus');
