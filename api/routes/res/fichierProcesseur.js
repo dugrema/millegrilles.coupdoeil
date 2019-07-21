bcca9695c6fa8ff15bc44f2e23a03c78cb87cee2
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-07-21 19:40:02 -0400

    Ajout SHA256.

--- api/routes/res/fichierProcesseur.js
+++ api/routes/res/fichierProcesseur.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const uuidv1 = require('uuid/v1');
 const request = require('request');
+const crypto = require('crypto');
+const rabbitMQ = require('./rabbitMQ');
 
 class ProcesseurUpload {
 
@@ -27,23 +29,66 @@ class ProcesseurUpload {
       };
 
       try {
-        // Encrypter fichiers
+
+        console.log("allo " + crypto.createHash('sha256').update('allo').digest('hex'));
+        console.log("bonsoir " + crypto.createHash('sha256').update('bonsoir').digest('hex'));
+        console.log("bonnenuit " + crypto.createHash('sha256').update('bonnenuit').digest('hex'));
+
+        // Encrypter fichiers, calculer SHA256 avant et apres
+        var sha256Clear = crypto.createHash('sha256');
+        // sha256Clear.setEncoding('binary');
 
         // Transmettre information au serveur via MQ
 
         // Uploader fichier vers central via PUT
         console.debug("PUT file " + fichier.path);
         fs.createReadStream(fichier.path)
+        .on('data', chunk=>{
+          sha256Clear.update(chunk);
+          console.log("------------");
+          process.stdout.write(chunk);
+          console.log("------------");
+        })
         .pipe(
           request.put(options, (err, httpResponse, body) => {
             if(err) throw err; // Attrapper erreur dans le catch plus bas
-
-            console.log("Put complete");
-            // console.log(err);
-            // console.log(body);
-            resolve(options);
           })
-        );
+        )
+        .on('end', ()=>{
+          let sha256ClearHash = sha256Clear.digest('hex');
+
+          console.log("Put complete, sending record to MQ");
+          let transactionNouvelleVersion = {
+            fuuid: fileUuid,
+            securite: 'prive',
+            repertoire: '/',
+            nom: fichier.originalname,
+            taille: fichier.size,
+            sha256: sha256ClearHash,
+            reception: {
+              methode: "coupdoeil",
+              "noeud": "public1.maple.mdugre.info"
+            }
+          }
+          console.debug("Transaction pour MQ");
+          console.debug(transactionNouvelleVersion);
+
+          resolve({transaction: transactionNouvelleVersion});
+
+          // rabbitMQ.singleton.transmettreTransactionFormattee(
+          //     transactionNouvelleVersion, 'millegrilles.domaines.GrosFichiers.nouvelleVersion')
+          //   .then( msg => {
+          //     console.log("Recu confirmation de transaction nouvelleVersion");
+          //     console.log(msg);
+          //
+          //     resolve({transaction: transactionNouvelleVersion, reponse: msg});
+          //   })
+          //   .catch( err => {
+          //     console.error("Erreur message");
+          //     console.error(err);
+          //     reject(err);
+          //   });
+        })
 
       } catch (err) {
         console.error("Erreur preparation fichier " + fichier.originalname);
