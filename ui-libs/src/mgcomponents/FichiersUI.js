5a6c6045fc04c92d3479e36fc7f808baae49e629
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-08-07 20:50:59 -0400

    Formattage liste icones avec images.

--- ui-libs/src/mgcomponents/FichiersUI.js
+++ ui-libs/src/mgcomponents/FichiersUI.js
@@ -3,39 +3,92 @@ import 'font-awesome/css/font-awesome.min.css';
 
 import './FichiersUI.css';
 
+var mapMimeType = {
+  'application/excel': 'fa-file-excel-o',
+  'application/json': 'fa-file-code-o',
+  'application/pdf': 'fa-file-pdf-o',
+  'application/text': 'fa-file-text-o',
+  'application/word': 'fa-file-word-o',
+  'image/jpeg': 'fa-file-image-o',
+}
+
 class PanneauFichiersIcones extends React.Component {
 
+  preparerRepertoires() {
+    let repertoires = this.props.repertoires;
+
+    // Extraire et trier les repertoires
+    let repertoiresTries = this.trierListe(repertoires);
+
+    // Faire le rendering
+    let listeRendered = [];
+    repertoiresTries.forEach(repertoire=>{
+      listeRendered.push(
+        <div key={repertoire.repertoire_uuid}>
+          <span className="fa-stack fa-2x">
+            <i className="fa fa-folder-open fa-stack-2x fond"></i>
+            <i className="fa fa-folder-open-o fa-stack-2x"></i>
+          </span>
+          <p>{repertoire.nom}</p>
+        </div>
+      );
+    });
+
+    return listeRendered;
+  }
+
+  preparerFichiers() {
+    let fichiers = this.props.fichiers;
+
+    // Extraire et trier les repertoires
+    let fichiersTries = this.trierListe(fichiers);
+
+    // Faire le rendering
+    let listeRendered = [];
+    fichiersTries.forEach(fichier=>{
+      let icone = this.determinerIconeFichier(fichier);
+      listeRendered.push(
+        <div key={fichier.uuid}>
+          <i className={icone}></i>
+          <p>{fichier.nom}</p>
+        </div>
+      );
+    });
+
+    return listeRendered;
+  }
+
+  determinerIconeFichier(fichier) {
+    let mimetype = fichier.mimetype;
+    let icone = mapMimeType[mimetype];
+    if(!icone) {
+      icone = 'fa-file-o'; // Par defaut un icone generique
+    }
+
+    return 'fa ' + icone + ' fa-5x';
+  }
+
+  trierListe(items) {
+    let listeTriee = [];
+    for(var item_uuid in items) {
+      let item = items[item_uuid];
+      listeTriee.push(item);
+    }
+    listeTriee.sort((a,b)=>{
+      let nomA=a.nom, nomB=b.nom;
+      return nomA.localeCompare(nomB);
+    })
+    return listeTriee;
+  }
+
   render() {
+    let repertoires = this.preparerRepertoires();
+    let fichiers = this.preparerFichiers();
+
     return (
       <div className="PanneauFichiersIcones">
-        <div>
-          <i className="fa fa-folder-open-o fa-5x"></i>
-          <p>nom_repertoire_et_fichier trop_longs fdas.txt</p>
-        </div>
-        <div>
-          <i className="fa fa-folder-open-o fa-5x"></i>
-          <p>nom_repertoire_et_fichier trop_longs fdas.txt</p>
-        </div>
-        <div>
-          <i className="fa fa-folder-open-o fa-5x"></i>
-          <p>nom_repertoire_et_fichier trop_longs fdas.txt</p>
-        </div>
-        <div>
-          <i className="fa fa-folder-open-o fa-5x"></i>
-          <p>nom_repertoire_et_fichier trop_longs fdas.txt</p>
-        </div>
-        <div>
-          <i className="fa fa-folder-open-o fa-5x"></i>
-          <p>nom_repertoire_et_fichier trop_longs fdas.txt</p>
-        </div>
-        <div>
-          <i className="fa fa-folder-open-o fa-5x"></i>
-          <p>nom_repertoire_et_fichier trop_longs fdas.txt</p>
-        </div>
-        <div>
-          <i className="fa fa-folder-open-o fa-5x"></i>
-          <p>nom_repertoire_et_fichier trop_longs fdas.txt</p>
-        </div>
+        {repertoires}
+        {fichiers}
       </div>
     );
   }
