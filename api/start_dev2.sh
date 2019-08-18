978aaaa82bfdf5dd945b05ed6534d6631b1784cd
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-08-17 21:51:11 -0400

    Fix pour demarrage avec certs.

--- api/start_dev2.sh
+++ api/start_dev2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CERT_FOLDER=/home/mathieu/certificates
+CERT_FOLDER=/opt/millegrilles/dev2/pki
 export MG_NOM_MILLEGRILLE=dev2
 
 # export COUPDOEIL_SESSION_TIMEOUT=15000
@@ -8,12 +8,12 @@ export MG_NOM_MILLEGRILLE=dev2
 # export MG_MQ_CERTFILE=$CERT_FOLDER/millegrilles/certs/dev2.maple.mdugre.info.cert.pem
 # export MG_MQ_KEYFILE=$CERT_FOLDER/millegrilles/privkeys/dev2.maple.mdugre.info.pem
 
-export MG_MQ_CAFILE=$CERT_FOLDER/millegrilles/certs/millegrille_dev2.maple.mdugre.info.cert.pem.rootchain
-export MG_MQ_CERTFILE=$CERT_FOLDER/millegrilles/certs/dev2.maple.mdugre.info.cert.pem.20190808
-export MG_MQ_KEYFILE=$CERT_FOLDER/millegrilles/privkeys/dev2.maple.mdugre.info.pem.20190808
+export MG_MQ_CAFILE=$CERT_FOLDER/certs/dev2_CA_chain.cert.pem
+export MG_MQ_CERTFILE=$CERT_FOLDER/certs/dev2_middleware.cert.pem
+export MG_MQ_KEYFILE=$CERT_FOLDER/keys/dev2_middleware.key.pem
 
-export CERT=$CERT_FOLDER/dev2.maple.mdugre.info/fullchain.pem
-export PRIVKEY=$CERT_FOLDER/dev2.maple.mdugre.info/privkey.pem
+export CERT=$CERT_FOLDER/certs/dev2_fullchain.cert.pem
+export PRIVKEY=$CERT_FOLDER/keys/dev2_middleware.key.pem
 export MG_MQ_URL=amqps://dev2.local:5673/$MG_NOM_MILLEGRILLE
 export PORT=3001
 
--- front-end/src/manifest.build.js
+++ front-end/src/manifest.build.js
@@ -1,5 +1,5 @@
 const build = {
-  date: '2019-08-11 16:15',
-  version: '1.9.1'
+  date: '2019-08-12 12:34',
+  version: '1.9.2'
 }
 module.exports = build;
