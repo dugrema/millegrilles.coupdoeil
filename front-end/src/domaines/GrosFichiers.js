85779c8b1d1653ac7653e76ae75c5bab73188041
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-08-23 10:48:09 -0400

    Refactoring des methods d'action, utilisation ellipsis en React.

--- front-end/src/domaines/GrosFichiers.js
+++ front-end/src/domaines/GrosFichiers.js
@@ -45,8 +45,10 @@ export class GrosFichiers extends React.Component {
 
   }
 
+  // Actions utiliees uniquement dans les pop-ups
   popupActions = {
 
+    // Creer un repertoire
     soumettreCreerRepertoire: (event) => {
       let uuidRepertoireParent = this.state.popupProps.popupCreerRepertoireValeurs.uuidRepertoireParent;
       let formulaire = event.currentTarget.form;
@@ -73,6 +75,8 @@ export class GrosFichiers extends React.Component {
     annulerCreerRepertoire: (event) => {
       this.setState({popupProps: {popupCreerRepertoireValeurs: null}});
     },
+
+    // Changer nom d'un repertoire
     soumettreChangerNomRepertoire: (event) => {
       let formulaire = event.currentTarget.form;
       let nouveauNom = formulaire.nouveauNom.value;
@@ -95,7 +99,251 @@ export class GrosFichiers extends React.Component {
     },
     annulerChangerNomRepertoire: (event) => {
       this.setState({popupRenommerRepertoireValeurs: null})
-    }
+    },
+
+    // Changer le nom d'un fichier
+    soumettreChangerNomFichier: (event) => {
+      let formulaire = event.currentTarget.form;
+      let nouveauNom = formulaire.nouveauNom.value;
+      let ancienNom = this.state.popupProps.popupRenommerFichierValeurs.nom;
+      let uuidFichier = this.state.popupProps.popupRenommerFichierValeurs.uuidFichier;
+
+      console.debug("Renommer fichier " + ancienNom + " a " + nouveauNom + ", uuid=" + uuidFichier);
+
+      if(nouveauNom !== ancienNom) {
+        // Transmettre message a MQ pour renommer le fichier
+        let transaction = {
+          "uuid": uuidFichier,
+          "nom": nouveauNom,
+        }
+        webSocketManager.transmettreTransaction(
+          'millegrilles.domaines.GrosFichiers.renommerFichier', transaction);
+      }
+
+      this.setState({popupRenommerFichierValeurs: null});
+    },
+    annulerChangerNomFichier: (event) => {
+      this.setState({popupRenommerFichierValeurs: null});
+    },
+
+    supprimerRepertoire: (event) => {
+      let uuidRepertoire = event.currentTarget.value;
+
+      let transaction = {
+        "repertoire_uuid": uuidRepertoire,
+      }
+      webSocketManager.transmettreTransaction(
+        'millegrilles.domaines.GrosFichiers.supprimerRepertoire', transaction)
+      .then(msg=>{
+        console.debug("Repertoire supprime " + uuidRepertoire);
+        let repertoireCourant = this.state.repertoireCourant;
+        if(repertoireCourant.repertoire_uuid === uuidRepertoire) {
+          // On va popper vers le repertoire parent
+          this.changerRepertoire(repertoireCourant.parent_id);
+        }
+      })
+      .catch(err=>{
+        console.error("Erreur suppression repertoire " + uuidRepertoire);
+      });
+    },
+
+    supprimerFichier: (event) => {
+      let uuidFichier = event.currentTarget.value;
+
+      let transaction = {
+        "uuid": uuidFichier,
+      }
+
+      webSocketManager.transmettreTransaction(
+        'millegrilles.domaines.GrosFichiers.supprimerFichier', transaction)
+      .then(msg=>{
+        console.debug("Fichier supprime: " + uuidFichier);
+      }).catch(err=>{
+        console.error("Erreur suppression fichier");
+        console.error(err);
+      });
+    },
+
+  }
+
+  // Actions utiliees dans le panneau des fichiers
+  fichierActions = {
+    upload: repertoireDestination => {
+      console.debug("Upload vers " + repertoireDestination);
+    },
+
+    telecharger: uuidfichier => {
+      // Trouver fichier dans information repertoire
+      let infoRepertoire = this.state.repertoireCourant || this.state.repertoireRacine;
+      console.debug(infoRepertoire);
+      var fichier = infoRepertoire.fichiers[uuidfichier];
+      console.debug("Telecharger fichier uuid: " + uuidfichier + ": " + fichier);
+      if(!fichier) {
+        throw new Error("Erreur fichier inconnu: " + uuidfichier)
+      }
+      let fuuid = fichier.fuuid_v_courante;
+      let nomFichier = fichier.nom;
+      let contentType = fichier.mimetype;
+
+      console.debug("1. Bouton clique pour fichier " + nomFichier);
+      let form = this.refFormulaireDownload.current;
+      let downloadUrl = this.state.downloadUrl;
+
+      console.debug("2. fuuide: " + fuuid);
+      webSocketManager.demanderTokenTransfert()
+      .then(token=>{
+        form.action = downloadUrl + "/" + nomFichier;
+        form.fuuid.value = fuuid;
+        form.nomfichier.value = nomFichier;
+        form.contenttype.value = contentType;
+        form.authtoken.value = token;
+        form.securite.value = fichier.securite;
+
+        console.debug("2. Submit preparation, download " + form.action + ", recu token " + form.authtoken.value);
+        form.submit(); // Token pret, submit.
+
+      })
+      .catch(err=>{
+        console.error("Erreur preparation download");
+        console.error(err);
+      })
+
+    },
+
+    copier: (repertoireDestination) => {
+      console.debug("Copier vers " + repertoireDestination);
+      let selection = this.state.elementsCopierDeplacer;
+      for(var uuid in selection) {
+        let infoitem = selection[uuid];
+        let typeitem = infoitem.type;
+        console.debug(typeitem + " " + uuid);
+      }
+    },
+
+    deplacer: (repertoireDestination) => {
+      console.debug("Deplacer vers " + repertoireDestination);
+      let selection = this.state.elementsCopierDeplacer;
+      for(var uuid in selection) {
+        let infoitem = selection[uuid];
+        let typeitem = infoitem.type;
+
+        if(typeitem === 'fichier') {
+          var transaction = {
+            "uuid": uuid,
+            "repertoire_uuid": repertoireDestination,
+          }
+          webSocketManager.transmettreTransaction(
+            'millegrilles.domaines.GrosFichiers.deplacerFichier', transaction);
+        } else if(typeitem === 'repertoire') {
+
+        }
+
+      }
+
+      // L'operation deplacement ne peut pas etre repetee.
+      this.setState({
+        elementsCopierDeplacer: null,
+        operationCopierDeplacer: null,
+      })
+
+    },
+
+    supprimer: (selection) => {
+      console.debug("Supprimer");
+      for(var uuid in selection) {
+        let infoitem = selection[uuid];
+        let typeitem = infoitem.type;
+        console.debug(typeitem + " " + uuid);
+      }
+    },
+
+    activerCopier: selection => {
+      this.setState({
+        elementsCopierDeplacer: selection,
+        operationCopierDeplacer: 'copier',
+      });
+    },
+
+    activerDeplacer: selection => {
+      this.setState({
+        elementsCopierDeplacer: selection,
+        operationCopierDeplacer: 'deplacer',
+      });
+    },
+
+    doubleclickRepertoire: (event) => {
+      let uuidRepertoire = event.currentTarget.dataset.repertoireuuid;
+      console.debug("Double click repertoire " + uuidRepertoire);
+      this.changerRepertoire(uuidRepertoire);
+    },
+
+    doubleclickFichier: (event) => {
+      let uuidFichier = event.currentTarget.dataset.fichieruuid;
+      console.debug("Double click fichier " + uuidFichier);
+      this.afficherProprietesFichier(uuidFichier);
+    },
+
+    ouvrir: (uuid, type) => {
+      console.debug("Ouvrir " + type + " " + uuid);
+      if(type === 'repertoire') {
+        this.changerRepertoire(uuid);
+      } else if(type === 'fichier') {
+        this.afficherProprietesFichier(uuid);
+      }
+    },
+
+    afficherPopupRenommer: (uuid, type) => {
+      console.debug("Renommer " + type + ", uuid " + uuid);
+      let repertoireCourant = this.state.repertoireCourant || this.state.repertoireRacine;
+
+      if(type === 'fichier') {
+        let nomFichier = repertoireCourant.fichiers[uuid].nom;
+        this.setState({popupProps: {popupRenommerFichierValeurs: {
+          nom: nomFichier,
+          uuidFichier: uuid,
+        }}});
+      } else if(type === 'repertoire') {
+        let nomRepertoire = repertoireCourant.repertoires[uuid].nom;
+        this.setState({popupRenommerRepertoireValeurs: {
+          nom: nomRepertoire,
+          uuidRepertoire: uuid,
+        }});
+      }
+
+    },
+
+  }
+
+  repertoireActions = {
+    afficherRepertoire: event => {
+      let bouton = event.currentTarget;
+      let uuidRepertoire = bouton.value;
+      this.changerRepertoire(uuidRepertoire);
+    },
+
+    deplacerSelection: (event) => {
+      let selection = this.state.selection;
+      let uuidRepertoireDestination = event.currentTarget.value;
+      console.debug("Deplacer selection vers " + uuidRepertoireDestination);
+      console.debug(selection);
+
+      for(var uuid in selection) {
+        let info = selection[uuid];
+        let type = info.type;
+        if(type === 'fichier') {
+          var transaction = {
+            "uuid": uuid,
+            "repertoire_uuid": uuidRepertoireDestination,
+          }
+          webSocketManager.transmettreTransaction(
+            'millegrilles.domaines.GrosFichiers.deplacerFichier', transaction);
+        } else if(type === 'repertoire') {
+
+        }
+      }
+
+      this.setState({'selection': {}}); // Clear
+    },
 
   }
 
@@ -125,48 +373,6 @@ export class GrosFichiers extends React.Component {
       console.debug(docsRecu);
       return docsRecu;  // Recuperer avec un then(resultats=>{})
     });
-  }
-
-  telecharger = uuidfichier => {
-    // Trouver fichier dans information repertoire
-    let infoRepertoire = this.state.repertoireCourant || this.state.repertoireRacine;
-    console.debug(infoRepertoire);
-    var fichier = infoRepertoire.fichiers[uuidfichier];
-    console.debug("Telecharger fichier uuid: " + uuidfichier + ": " + fichier);
-    if(!fichier) {
-      throw new Error("Erreur fichier inconnu: " + uuidfichier)
-    }
-    let fuuid = fichier.fuuid_v_courante;
-    let nomFichier = fichier.nom;
-    let contentType = fichier.mimetype;
-
-    console.debug("1. Bouton clique pour fichier " + nomFichier);
-    let form = this.refFormulaireDownload.current;
-    let downloadUrl = this.state.downloadUrl;
-
-    console.debug("2. fuuide: " + fuuid);
-    webSocketManager.demanderTokenTransfert()
-    .then(token=>{
-      form.action = downloadUrl + "/" + nomFichier;
-      form.fuuid.value = fuuid;
-      form.nomfichier.value = nomFichier;
-      form.contenttype.value = contentType;
-      form.authtoken.value = token;
-      form.securite.value = fichier.securite;
-
-      console.debug("2. Submit preparation, download " + form.action + ", recu token " + form.authtoken.value);
-      form.submit(); // Token pret, submit.
-
-    })
-    .catch(err=>{
-      console.error("Erreur preparation download");
-      console.error(err);
-    })
-
-  }
-
-  upload = repertoireDestination => {
-    console.debug("Upload vers " + repertoireDestination);
   }
 
   afficherProprietesFichier(uuidFichier) {
@@ -206,94 +412,6 @@ export class GrosFichiers extends React.Component {
     }
   }
 
-  activerCopier = selection => {
-    this.setState({
-      elementsCopierDeplacer: selection,
-      operationCopierDeplacer: 'copier',
-    });
-  }
-
-  activerDeplacer = selection => {
-    this.setState({
-      elementsCopierDeplacer: selection,
-      operationCopierDeplacer: 'deplacer',
-    });
-  }
-
-  doubleclickRepertoire = (event) => {
-    let uuidRepertoire = event.currentTarget.dataset.repertoireuuid;
-    console.debug("Double click repertoire " + uuidRepertoire);
-    this.changerRepertoire(uuidRepertoire);
-  }
-
-  doubleclickFichier = (event) => {
-    let uuidFichier = event.currentTarget.dataset.fichieruuid;
-    console.debug("Double click fichier " + uuidFichier);
-    this.afficherProprietesFichier(uuidFichier);
-  }
-
-  copier = (repertoireDestination) => {
-    console.debug("Copier vers " + repertoireDestination);
-    let selection = this.state.elementsCopierDeplacer;
-    for(var uuid in selection) {
-      let infoitem = selection[uuid];
-      let typeitem = infoitem.type;
-      console.debug(typeitem + " " + uuid);
-    }
-  }
-
-  deplacer = (repertoireDestination) => {
-    console.debug("Deplacer vers " + repertoireDestination);
-    let selection = this.state.elementsCopierDeplacer;
-    for(var uuid in selection) {
-      let infoitem = selection[uuid];
-      let typeitem = infoitem.type;
-
-      if(typeitem === 'fichier') {
-        var transaction = {
-          "uuid": uuid,
-          "repertoire_uuid": repertoireDestination,
-        }
-        webSocketManager.transmettreTransaction(
-          'millegrilles.domaines.GrosFichiers.deplacerFichier', transaction);
-      } else if(typeitem === 'repertoire') {
-
-      }
-
-    }
-
-    // L'operation deplacement ne peut pas etre repetee.
-    this.setState({
-      elementsCopierDeplacer: null,
-      operationCopierDeplacer: null,
-    })
-
-  }
-
-  supprimer = (selection) => {
-    console.debug("Supprimer");
-    for(var uuid in selection) {
-      let infoitem = selection[uuid];
-      let typeitem = infoitem.type;
-      console.debug(typeitem + " " + uuid);
-    }
-  }
-
-  ouvrir = (uuid, type) => {
-    console.debug("Ouvrir " + type + " " + uuid);
-    if(type === 'repertoire') {
-      this.changerRepertoire(uuid);
-    } else if(type === 'fichier') {
-      this.afficherProprietesFichier(uuid);
-    }
-  }
-
-  afficherRepertoire = event => {
-    let bouton = event.currentTarget;
-    let uuidRepertoire = bouton.value;
-    this.changerRepertoire(uuidRepertoire);
-  }
-
   retourRepertoireFichier = (event) => {
     this.setState({'fichierCourant': null});
   }
@@ -306,88 +424,6 @@ export class GrosFichiers extends React.Component {
 
   deplacerRepertoire = (event) => {
 
-  }
-
-  afficherPopupRenommer = (uuid, type) => {
-    console.debug("Renommer " + type + ", uuid " + uuid);
-    let repertoireCourant = this.state.repertoireCourant || this.state.repertoireRacine;
-
-    if(type === 'fichier') {
-      let nomFichier = repertoireCourant.fichiers[uuid].nom;
-      this.setState({popupProps: {popupRenommerFichierValeurs: {
-        nom: nomFichier,
-        uuidFichier: uuid,
-      }}});
-    } else if(type === 'repertoire') {
-      let nomRepertoire = repertoireCourant.repertoires[uuid].nom;
-      this.setState({popupRenommerRepertoireValeurs: {
-        nom: nomRepertoire,
-        uuidRepertoire: uuid,
-      }});
-    }
-
-  }
-
-  supprimerRepertoire = (event) => {
-    let uuidRepertoire = event.currentTarget.value;
-
-    let transaction = {
-      "repertoire_uuid": uuidRepertoire,
-    }
-    webSocketManager.transmettreTransaction(
-      'millegrilles.domaines.GrosFichiers.supprimerRepertoire', transaction)
-    .then(msg=>{
-      console.debug("Repertoire supprime " + uuidRepertoire);
-      let repertoireCourant = this.state.repertoireCourant;
-      if(repertoireCourant.repertoire_uuid === uuidRepertoire) {
-        // On va popper vers le repertoire parent
-        this.changerRepertoire(repertoireCourant.parent_id);
-      }
-    })
-    .catch(err=>{
-      console.error("Erreur suppression repertoire " + uuidRepertoire);
-    });
-  }
-
-  deplacerSelection = (event) => {
-    let selection = this.state.selection;
-    let uuidRepertoireDestination = event.currentTarget.value;
-    console.debug("Deplacer selection vers " + uuidRepertoireDestination);
-    console.debug(selection);
-
-    for(var uuid in selection) {
-      let info = selection[uuid];
-      let type = info.type;
-      if(type === 'fichier') {
-        var transaction = {
-          "uuid": uuid,
-          "repertoire_uuid": uuidRepertoireDestination,
-        }
-        webSocketManager.transmettreTransaction(
-          'millegrilles.domaines.GrosFichiers.deplacerFichier', transaction);
-      } else if(type === 'repertoire') {
-
-      }
-    }
-
-    this.setState({'selection': {}}); // Clear
-  }
-
-  supprimerFichier = (event) => {
-    let uuidFichier = event.currentTarget.value;
-
-    let transaction = {
-      "uuid": uuidFichier,
-    }
-
-    webSocketManager.transmettreTransaction(
-      'millegrilles.domaines.GrosFichiers.supprimerFichier', transaction)
-    .then(msg=>{
-      console.debug("Fichier supprime: " + uuidFichier);
-    }).catch(err=>{
-      console.error("Erreur suppression fichier");
-      console.error(err);
-    });
   }
 
   processMessage = (routingKey, doc) => {
@@ -414,31 +450,6 @@ export class GrosFichiers extends React.Component {
         this.setState({fichierCourant: doc});
       }
     }
-  }
-
-  soumettreChangerNomFichier = (event) => {
-    let formulaire = event.currentTarget.form;
-    let nouveauNom = formulaire.nouveauNom.value;
-    let ancienNom = this.state.popupProps.popupRenommerFichierValeurs.nom;
-    let uuidFichier = this.state.popupProps.popupRenommerFichierValeurs.uuidFichier;
-
-    console.debug("Renommer fichier " + ancienNom + " a " + nouveauNom + ", uuid=" + uuidFichier);
-
-    if(nouveauNom !== ancienNom) {
-      // Transmettre message a MQ pour renommer le fichier
-      let transaction = {
-        "uuid": uuidFichier,
-        "nom": nouveauNom,
-      }
-      webSocketManager.transmettreTransaction(
-        'millegrilles.domaines.GrosFichiers.renommerFichier', transaction);
-    }
-
-    this.setState({popupRenommerFichierValeurs: null});
-  }
-
-  annulerChangerNomFichier = (event) => {
-    this.setState({popupRenommerFichierValeurs: null});
   }
 
   componentDidMount() {
@@ -476,7 +487,6 @@ export class GrosFichiers extends React.Component {
             fichierCourant={this.state.fichierCourant}
             downloadUrl={this.state.downloadUrl}
             retourRepertoireFichier={this.retourRepertoireFichier}
-            afficherChangerNomFichier={this.afficherChangerNomFichier}
             />
         </div>
       )
@@ -491,27 +501,16 @@ export class GrosFichiers extends React.Component {
           <NavigationRepertoire
             repertoireCourant={repertoireCourant}
             downloadUrl={this.state.downloadUrl}
-            afficherChangerNom={this.afficherChangerNom}
+
             afficherPopupCreerRepertoire={this.afficherPopupCreerRepertoire}
-            afficherRepertoire={this.afficherRepertoire}
-            afficherPopupRenommerRepertoire={this.afficherPopupRenommerRepertoire}
-            deplacerSelection={this.deplacerSelection}
+            {...this.repertoireActions}
             />
           <PanneauFichiersIcones
             repertoire={repertoireCourant}
-            doubleclickRepertoire={this.doubleclickRepertoire}
-            doubleclickFichier={this.doubleclickFichier}
-            copier={this.copier}
-            deplacer={this.deplacer}
-            supprimer={this.supprimer}
-            ouvrir={this.ouvrir}
-            telecharger={this.telecharger}
-            upload={this.upload}
-            activerCopier={this.activerCopier}
-            activerDeplacer={this.activerDeplacer}
             operationCopierDeplacer={this.state.operationCopierDeplacer}
+
             creerRepertoire={this.afficherPopupCreerRepertoire}
-            renommer={this.afficherPopupRenommer}
+            {...this.fichierActions}
             />
 
         </div>
@@ -522,9 +521,7 @@ export class GrosFichiers extends React.Component {
       <div className="w3-col m9">
         <div className="w3-row-padding">
           <div className="w3-col m12">
-            <h1>Domaine GrosFichiers</h1>
             <div>
-              <Accueil/>
               {affichagePrincipal}
             </div>
           </div>
