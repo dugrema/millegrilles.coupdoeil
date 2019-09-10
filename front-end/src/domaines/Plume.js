bf87e95d29df2d10fc749b9690a6e6c3b6bfdf2c
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-09 22:01:03 -0400

    Travail sur Plume.

--- front-end/src/domaines/Plume.js
+++ front-end/src/domaines/Plume.js
@@ -6,6 +6,8 @@ import './Plume.css';
 export class Plume extends React.Component {
 
   state = {
+    editionDocument: null,
+    affichageDocument: null,
   }
 
   fonctionsEdition = {
@@ -21,6 +23,9 @@ export class Plume extends React.Component {
   }
 
   fonctionsGestion = {
+    nouveauDocument: event => {
+      this.setState({editionDocument: 'nouveau'});
+    },
     editerDocument: event => {
 
     },
@@ -42,25 +47,32 @@ export class Plume extends React.Component {
   }
 
   render() {
+
+    let contenu = null;
+    if(this.state.editionDocument) {
+      contenu = (
+        <PlumeEditeur
+          fonctionsEdition={this.fonctionsEdition}/>
+      )
+    } else {
+      contenu = (
+        <div className="w3-col m12">
+          <div className="w3-card w3-round w3-white">
+            <div className="w3-container w3-padding">
+              <h2 className="w3-opacity">Plume</h2>
+
+              <button
+                onClick={this.fonctionsGestion.nouveauDocument}>Nouveau document</button>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
     return (
       <div className="w3-col m9">
         <div className="w3-row-padding">
-          <div className="w3-col m12">
-
-            <div className="w3-card w3-round w3-white">
-              <div className="w3-container w3-padding">
-                <h2 className="w3-opacity">Plume</h2>
-              </div>
-            </div>
-
-            <div className="w3-card w3-round w3-white">
-              <div className="w3-container w3-padding">
-                <PlumeEditeur
-                  fonctionsEdition={this.fonctionsEdition}/>
-              </div>
-            </div>
-
-          </div>
+          {contenu}
         </div>
       </div>
     )
@@ -74,14 +86,51 @@ class PlumeEditeur extends React.Component {
     titre: null,
     dateCreation: null,
     dateModification: null,
-    categorie: null,
+    categories: null,
     texte: null,
+    securite: '2.prive',
+  }
+
+  afficherEntete() {
+    return (
+      <div className="w3-card w3-round w3-white">
+        <div className="w3-container w3-padding">
+          <h2 className="w3-opacity">Plume</h2>
+        </div>
+      </div>
+    );
+  }
+
+  afficherInfoDoc() {
+    return (
+      <div className="w3-card w3-round w3-white">
+        <div className="w3-container w3-padding">
+          <div>Titre document : </div>
+          <div>Date création : </div>
+          <div><span title="(tags - minuscules, séparés par des espaces">Catégories</span> : </div>
+        </div>
+      </div>
+    );
+  }
+
+  afficherQuillEditeur() {
+    return (
+      <div className="w3-card w3-round w3-white">
+        <div className="w3-container w3-padding">
+          <ReactQuill value={this.state.texte}
+                      onChange={this.props.fonctionsEdition.changementQuill} />
+        </div>
+      </div>
+    );
   }
 
   render() {
     return (
-      <ReactQuill value={this.state.texte}
-                  onChange={this.props.fonctionsEdition.changementQuill} />
+      <div className="w3-col m12">
+        {this.afficherEntete()}
+        {this.afficherInfoDoc()}
+        {this.afficherQuillEditeur()}
+      </div>
     );
   }
 
