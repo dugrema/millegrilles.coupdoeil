b8013a794129c556ec6bd2f16d48ee78ecbf16e7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-11-03 19:29:05 +0000

    Affichage des certs root, millegrille dans Pki.
    Update layout pour signer certs, ajout Noeud et Noeud Public comme
    options.

--- front-end/src/domaines/Pki.js
+++ front-end/src/domaines/Pki.js
@@ -4,6 +4,7 @@ import Checkbox from "../mgcomponents/Checkbox";
 import webSocketManager from '../WebSocketManager';
 // import {dateformatter} from '../formatters'
 import './Pki.css';
+import {dateformatter} from '../formatters';
 
 // const domaine = 'millegrilles.domaines.Pki';
 // const libelle_signerNoeud = 'signer.noeud';
@@ -88,7 +89,9 @@ export class SignerNoeud extends React.Component {
             <div className="w3-col m8 champ">
               <select value={this.state.domaine} onChange={this.changementDomaine}>
                 <option value="">Choisir un domaine</option>
-                <option value="SenseursPassifs">SenseursPassifs</option>
+                <option value="Noeud">Noeud</option>
+                <option value="NoeudPublication">Noeud de publication</option>
+                <option value="SenseursPassifs">Senseurs Passifs</option>
               </select>
             </div>
           </div>
@@ -103,7 +106,7 @@ export class SignerNoeud extends React.Component {
             <div className="w3-col m12">
               <textarea
                 value={this.state.requeteCsr} onChange={this.changementRequeteCsr}
-                cols="60" rows="20" />
+                rows="20" />
             </div>
           </div>
 
@@ -145,7 +148,7 @@ export class SignerNoeud extends React.Component {
     }
 
     return (
-      <div className="w3-col m9">
+      <div className="w3-col m12">
         <div className="w3-row-padding">
 
           <div className="w3-card w3-round w3-white">
@@ -153,8 +156,8 @@ export class SignerNoeud extends React.Component {
               <h2 className="w3-opacity">Signer un certificat de noeud</h2>
 
               <p>
-                Cette page permet de signer une requete de certificat generee
-                sur un noeud qui doit se connecter a la MilleGrille
+                Cette page permet de signer une requête de certificat générée
+                sur un noeud qui doit se connecter à la MilleGrille
               </p>
 
             </div>
@@ -173,7 +176,10 @@ export class SignerNoeud extends React.Component {
 
 export class RenouvellerCertificats extends React.Component {
 
-  LISTE_MIDDLEWARE = ["mq", "nginx", "coupdoeil"];
+  LISTE_MIDDLEWARE = [
+    "mq", "nginx", "coupdoeil", "ceduleur", "domaines",
+    "fichiers", "maitredescles", "mongo", "mongoexpress", "publicateur",
+    "transaction", "vitrine"];
 
   state = {
     renouvellementMiddlewareTransmis: '',
@@ -337,6 +343,201 @@ export class RenouvellerCertificats extends React.Component {
 
 }
 
+export class AfficherCertificatsRoot extends React.Component {
+
+  state = {
+    certificatsRoot: [],
+    certificatsMillegrille: [],
+    parametresPublics: null,
+  }
+
+  componentDidMount() {
+    let requeteCerts =  {
+      'requetes': [
+        {
+          'filtre': {
+            '_mg-libelle': 'certificat.root',
+            'chaine_complete': true,
+          }
+        },
+        {
+          'filtre': {
+            '_mg-libelle': 'certificat.millegrille',
+            'chaine_complete': true,
+          }
+        },
+      ]};
+
+    // Requete pour charger certificats root du domaine
+    let domaine = 'requete.millegrilles.domaines.Pki.certificatsRoot';
+
+    // Enregistrer les routingKeys, demander le document initial.
+    webSocketManager.transmettreRequete(domaine, requeteCerts)
+    .then( docInitial => {
+      let certificatsRoot = docInitial[0];
+      let certificatsMillegrille = docInitial[1];
+      this.setState({certificatsRoot, certificatsMillegrille});
+    })
+    .catch( err=>{
+      console.error("Erreur chargement document initial");
+      console.error(err);
+    });
+
+    // Aller chercher le URL public de la millegrille
+    let requeteParametres =  {
+      'requetes': [{
+          'filtre': {
+            '_mg-libelle': 'publique.configuration',
+          }
+      }]};
+    // Requete pour charger certificats root du domaine
+    let domaineParametres = 'requete.millegrilles.domaines.Parametres.publique.configuration';
+
+    // Enregistrer les routingKeys, demander le document initial.
+    webSocketManager.transmettreRequete(domaineParametres, requeteParametres)
+    .then( docParametres => {
+      let parametresPublics = docParametres[0][0];
+      this.setState({parametresPublics});
+    })
+    .catch( err=>{
+      console.error("Erreur chargement parametres publics");
+      console.error(err);
+    });
+  }
+
+  genererListeRoots() {
+    let roots = null;
+    if(this.state.certificatsRoot) {
+      roots = this.genererListeCertificats(this.state.certificatsRoot);
+    }
+    return roots;
+  }
+
+  genererListeCertificatsMilleGrille() {
+    let certs = null;
+    if(this.state.certificatsMillegrille) {
+      certs = this.genererListeCertificats(this.state.certificatsMillegrille);
+    }
+    return certs;
+  }
+
+  genererListeCertificats(liste) {
+    let certificats = [];
+
+    for(let idx in liste) {
+      let certRoot = liste[idx];
+      certificats.push(
+        <div key={certRoot.fingerprint} className="w3-card w3-round w3-white">
+          <div className="w3-container w3-padding formulaire">
+            <div>
+              <div className="w3-col m4 label">MilleGrille</div>
+              <div className="w3-col m8">{certRoot.sujet.organizationName}</div>
+            </div>
+            <div>
+              <div className="w3-col m4 label">Fingerprint</div>
+              <div className="w3-col m8">{certRoot.fingerprint}</div>
+            </div>
+            <div>
+              <div className="w3-col m4 label">Authority key</div>
+              <div className="w3-col m8">{certRoot.authority_key}</div>
+            </div>
+            <div>
+              <div className="w3-col m4 label">Subject key</div>
+              <div className="w3-col m8">{certRoot.subject_key}</div>
+            </div>
+            <div>
+              <div className="w3-col m4 label">Not valid before</div>
+              <div className="w3-col m8">{dateformatter.format_datetime(certRoot.not_valid_before)}</div>
+            </div>
+            <div>
+              <div className="w3-col m4 label">Not valid after</div>
+              <div className="w3-col m8">{dateformatter.format_datetime(certRoot.not_valid_after)}</div>
+            </div>
+            <div>
+              <div className="w3-col m12">
+                <pre>
+                  {certRoot.certificat_pem}
+                </pre>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    return certificats;
+  }
+
+  contenu() {
+    let nomMilleGrille = this.props.documentIdMillegrille.nom_millegrille;
+    let urlBase = this.props.documentIdMillegrille.adresse_url_base;
+    var urlLocal = null;
+
+    if(this.state.parametresPublics && this.state.parametresPublics['url_web']) {
+      urlLocal = (<span>(<a href={'https://'+ urlBase + '/certs/' + nomMilleGrille + '.CA.cert.pem'}>local</a>)</span>);
+      urlBase = this.state.parametresPublics['url_web'];
+    }
+    let lienCAs = 'https://'+ urlBase + '/certs/' + nomMilleGrille + '.CA.cert.pem';
+
+    let entete = (
+      <div className="w3-card w3-round w3-white">
+        <div className="w3-container w3-padding">
+          <h2 className="w3-col m12 w3-opacity">Liste de référence des certificats de la MilleGrille</h2>
+
+          <p>
+            Les certificats racines (premiers dans la liste) sont ceux qui doivent
+            être utilisés pour valider les chaines.
+          </p>
+
+          <div className="w3-col m12">
+            <p>
+              Noter que les fichiers de certificats racines sont disponibles via lien web :<br/>
+              <a href={lienCAs}>{lienCAs}</a> {urlLocal}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+
+    let enteteCertsMilleGrilles = (
+      <div className="w3-card w3-round w3-white">
+        <div className="w3-container w3-padding">
+          <h2 className="w3-col m12 w3-opacity">Certificats intermédiaires</h2>
+
+          <p>
+            Les certificats suivants sont des certificats intermédiaires
+            fournis en référence.
+          </p>
+
+        </div>
+      </div>
+    );
+
+    return (
+      <div className="w3-col m12">
+        {entete}
+        {this.genererListeRoots()}
+
+        {enteteCertsMilleGrilles}
+        {this.genererListeCertificatsMilleGrille()}
+      </div>
+    );
+
+  }
+
+  render() {
+    return (
+
+      <div className="w3-col m12">
+        <div className="w3-row-padding">
+            {this.contenu()}
+        </div>
+      </div>
+    );
+  }
+
+}
+
 export class Pki extends React.Component {
 
   state = {
@@ -346,6 +547,7 @@ export class Pki extends React.Component {
   sousPages = {
     'SignerNoeud': SignerNoeud,
     'RenouvellerCertificats': RenouvellerCertificats,
+    'AfficherCertificatsRoot': AfficherCertificatsRoot,
   }
 
   fonctionsNavigation = {
@@ -365,6 +567,11 @@ export class Pki extends React.Component {
           <h2 className="w3-opacity">Gestion des certificats (Public Key Infrastructure)</h2>
 
           <ul>
+            <li>
+              <button className="aslink" onClick={this.fonctionsNavigation.afficherEcran} value="AfficherCertificatsRoot">
+                Afficher certificats racines
+              </button>
+            </li>
             <li>
               <button className="aslink" onClick={this.fonctionsNavigation.afficherEcran} value="SignerNoeud">
                 Signer un certificat de noeud
@@ -388,7 +595,9 @@ export class Pki extends React.Component {
 
       contenu = (
         <ModuleGestion
-          {...this.fonctionsNavigation} />
+          {...this.fonctionsNavigation}
+          documentIdMillegrille={this.props.documentIdMillegrille}
+          />
       );
     } else {
       contenu = (
