37bb93fe33750aec0d2abffcbe6cbc51d877d0e6
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-26 22:07:20 +0000

    Debut refact pour hebergement coupdoeil

--- api/start_hebergement.sh
+++ api/start_hebergement.sh
@@ -11,15 +11,15 @@ CERT_FOLDER=/home/mathieu/mgdev/certs
 
 # export COUPDOEIL_SESSION_TIMEOUT=15000
 export MG_MQ_CAFILE=$CERT_FOLDER/pki.millegrille.cert
-export MG_MQ_CERTFILE=$CERT_FOLDER/pki.coupdoeil.fullchain
-export MG_MQ_KEYFILE=$CERT_FOLDER/pki.coupdoeil.key
+export MG_MQ_CERTFILE=$CERT_FOLDER/pki.heb_coupdoeil.fullchain
+export MG_MQ_KEYFILE=$CERT_FOLDER/pki.heb_coupdoeil.key
 
 export CERT=~/.acme.sh/mg-dev3.maple.maceroc.com/fullchain.cer
 export PRIVKEY=~/.acme.sh/mg-dev3.maple.maceroc.com/mg-dev3.maple.maceroc.com.key
 export MG_MQ_URL=amqps://$HOST.local:5673/$MG_IDMG
-export PORT=3001
+export PORT=3006
 
 export MG_HTTPPROXY_SECURE=false
 export MG_CONSIGNATION_HTTP=https://$HOST:3003
 
-npm start
+npm run-script hebergement
