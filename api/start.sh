ca79d8847d0a0085588dc60ee153a67ee6e461b5
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-27 22:18:56 +0000

    Changement nom param cert/key web

--- api/start.sh
+++ api/start.sh
@@ -13,8 +13,8 @@ export MG_MQ_CAFILE=$CERT_FOLDER/pki.millegrille.cert
 export MG_MQ_CERTFILE=$CERT_FOLDER/pki.coupdoeil.cert
 export MG_MQ_KEYFILE=$CERT_FOLDER/pki.coupdoeil.key
 
-export CERT=~/.acme.sh/mg-dev3.maple.maceroc.com/fullchain.cer
-export PRIVKEY=~/.acme.sh/mg-dev3.maple.maceroc.com/mg-dev3.maple.maceroc.com.key
+export WEB_CERT=~/.acme.sh/mg-dev3.maple.maceroc.com/fullchain.cer
+export WEB_KEY=~/.acme.sh/mg-dev3.maple.maceroc.com/mg-dev3.maple.maceroc.com.key
 export MG_MQ_URL=amqps://$HOST.local:5673/$MG_IDMG
 export PORT=3001
 
