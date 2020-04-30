1129d248e86ea71e7bb498da569eb6656cdedbfd
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-04-30 20:07:43 +0000

    Demarrage hebergement web

--- api/routes/info.js
+++ api/routes/info.js
@@ -57,35 +57,42 @@ function initialiserInfo(sessionManagement, opts) {
   // .then(info=>{
     router.get('/info.json', (req, res, next)=>{
 
-      const rabbitMQ = sessionManagement.fctRabbitMQParIdmg(idmg);
+      if(!modeHebergement) {
+        const rabbitMQ = sessionManagement.fctRabbitMQParIdmg(idmg);
 
-      const routing = 'millegrilles.domaines.Principale';
-      const filtre = {"_mg-libelle": "cles"};
-      rabbitMQ.get_document(
-        'millegrilles.domaines.Principale', filtre)
-      .then(doc=>{
+        const routing = 'millegrilles.domaines.Principale';
+        const filtre = {"_mg-libelle": "cles"};
+        rabbitMQ.get_document(
+          'millegrilles.domaines.Principale', filtre)
+        .then(doc=>{
 
-        const infoCopie = Object.assign({}, info);
+          const infoCopie = Object.assign({}, info);
 
-        if (!doc || doc.empreinte_absente) {
-          infoCopie.empreinte = false;
-        } else {
-          infoCopie.empreinte = true;
-        }
+          if (!doc || doc.empreinte_absente) {
+            infoCopie.empreinte = false;
+          } else {
+            infoCopie.empreinte = true;
+          }
 
-        const reponse = JSON.stringify(infoCopie);
-        console.debug(reponse);
+          const reponse = JSON.stringify(infoCopie);
+          // console.debug(reponse);
 
+          res.setHeader('Content-Type', 'application/json');
+          res.end(reponse);
+
+        })
+        .catch(err=>{
+          // On n'a pas recu l'information, on continue.
+          console.warn("Erreur reception info/config.json");
+          console.warn(err);
+          res.sendStatus(500);
+        });
+      } else {
+        // Mode hebergement
+        const reponse = JSON.stringify(info);
         res.setHeader('Content-Type', 'application/json');
         res.end(reponse);
-
-      })
-      .catch(err=>{
-        // On n'a pas recu l'information, on continue.
-        console.warn("Erreur reception info/config.json");
-        console.warn(err);
-        res.sendStatus(500);
-      });
+      }
 
     });
   // });
--- api/start_hebergement.sh
+++ api/start_hebergement.sh
@@ -13,10 +13,10 @@ export MG_MQ_CAFILE=$CERT_FOLDER/pki.millegrille.cert
 export HEB_CERTFILE=$CERT_FOLDER/pki.heb_coupdoeil.cert
 export HEB_KEYFILE=$CERT_FOLDER/pki.heb_coupdoeil.key
 
-export CERT=~/.acme.sh/mg-dev3.maple.maceroc.com/fullchain.cer
-export PRIVKEY=~/.acme.sh/mg-dev3.maple.maceroc.com/mg-dev3.maple.maceroc.com.key
+export WEB_CERT=~/.acme.sh/mg-dev3.maple.maceroc.com/fullchain.cer
+export WEB_KEY=~/.acme.sh/mg-dev3.maple.maceroc.com/mg-dev3.maple.maceroc.com.key
 export MG_MQ_URL=amqps://$HOST:5673
-export PORT=3006
+export PORT=3001
 
 export MG_HTTPPROXY_SECURE=false
 export MG_CONSIGNATION_HTTP=https://$HOST:3003
