94204dec1afea30adfb008b0fa7d819837ac6432
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-05-03 23:50:58 +0000

    Chargement liste millegrilles hebergees

--- front-end/src/domaines/Hebergement.js
+++ front-end/src/domaines/Hebergement.js
@@ -51,6 +51,7 @@ export class Hebergement extends React.Component {
     let contenu;
     if(sousEcran === 'millegrillesHebergees') {
       contenu = <MillegrillesHebergees
+                  webSocketManager={this.props.webSocketManager}
                   fonctionsNavigation={{afficherEcran: this.afficherEcran, retourInitiale: this.retourInitiale}}
                   />;
     } else {
@@ -93,7 +94,84 @@ function PageInitiale(props) {
 
 class MillegrillesHebergees extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      listeMillegrilles: [],
+    }
+    this.subscriptions = [
+        'noeuds.source.millegrilles_domaines_Hebergement.documents.millegrille.hebergee',
+    ];
+  }
+
+  componentDidMount() {
+    // Enregistrer les routingKeys de documents
+    this.props.webSocketManager.subscribe(this.subscriptions, this.processMessage);
+
+    // Aller chercher la liste des MilleGrilles hebergees
+    const routing = 'requete.millegrilles.domaines.Hebergement.requeteMilleGrillesHebergees';
+    const requete = {}
+    this.props.webSocketManager.transmettreRequete(routing, requete)
+    .then(reponse=>{
+      console.debug("Reponse millegrilles hebergees");
+      console.debug(reponse);
+      this.setState({listeMillegrilles: reponse});
+    });
+
+  }
+
+  componentWillUnmount() {
+    this.props.webSocketManager.unsubscribe(this.subscriptions);
+  }
+
+  processMessage = (routing, message) => {
+    console.debug("Message recu, routing : %s", routing);
+    console.debug(message);
+  }
+
   render() {
+
+    // const liste = [
+    //   {'idmg': '2zHt8UFJhAdH8XzcPfb4Evsn4kcZrvZ7fxBT97a', 'etat': 'actif'},
+    //   {'idmg': '3M87pZxVVWbT1dVLeRarQnge1mvADTs4trG7Caa', 'etat': 'actif'},
+    // ];
+    const liste = this.state.listeMillegrilles.sort((a,b)=>{
+      if(a===b) return 0;
+      if(a.idmg && b.idmg) return a.idmg.localeCompare(b.idmg);
+      if(!b) return 1;
+      return -1;
+    })
+
+    const listeRendered = liste.map(millegrille=>{
+
+      const boutons = []
+      const actif = millegrille.etat === 'actif';
+      boutons.push(
+        <Button key="activer" variant="secondary" disabled={actif}><Trans>global.activer</Trans></Button>
+      );
+      boutons.push(
+        <Button key="desactiver" variant="secondary" disabled={!actif}><Trans>global.desactiver</Trans></Button>
+      );
+      boutons.push(
+        <Button key="supprimer" variant="danger"><Trans>global.supprimer</Trans></Button>
+      );
+
+      return (
+        <Row key={millegrille.idmg}>
+          <Col lg={6}>
+            {millegrille.idmg}
+          </Col>
+          <Col lg={1}>
+            {millegrille.etat}
+          </Col>
+          <Col lg={5}>
+            <ButtonGroup aria-label="Operations MilleGrille">
+              {boutons}
+            </ButtonGroup>
+          </Col>
+        </Row>
+      );
+    });
 
     return(
       <div>
@@ -105,15 +183,20 @@ class MillegrillesHebergees extends React.Component {
 
         <Feuille>
 
-          <Row>
-            <Col>
-              Boutons: ajouter millegrille hebergee
-            </Col>
-          </Row>
+          <div className="table">
+            <Row className="table-header">
+              <Col lg={6}><Trans>hebergement.millegrillesHebergees.idmg</Trans></Col>
+              <Col lg={1}><Trans>hebergement.millegrillesHebergees.etat</Trans></Col>
+              <Col lg={5}><Trans>hebergement.millegrillesHebergees.actions</Trans></Col>
+            </Row>
+            {listeRendered}
+          </div>
 
           <Row>
             <Col>
-              Liste millegrilles hebergees, boutons : activer, desactiver, supprimer
+              <ButtonGroup aria-label="Operations MilleGrilles hebergees">
+                <Button variant="primary"><Trans>global.ajouter</Trans></Button>
+              </ButtonGroup>
             </Col>
           </Row>
 
