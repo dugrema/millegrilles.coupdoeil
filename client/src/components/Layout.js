46c53caa2ad5e6ba6929239fd107ee15d498be8b
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-05-31 18:42:25 +0000

    Ajout layout haut niveau, retour vers /millegrilles

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,15 +1,68 @@
 import React from 'react'
+import { Nav, Navbar, NavDropdown, Container, Row, Col} from 'react-bootstrap';
+import { Trans, Translation, withTranslation } from 'react-i18next';
+
+import './Layout.css'
 
 export function LayoutCoudpoeil(props) {
 
   return (
     <div>
-      <h1>Coup D'Oeil</h1>
-
-      <div>
-        {props.page}
-      </div>
+      <Entete/>
+      <Contenu page={props.page}/>
+      <Footer/>
     </div>
   )
 
+}
+
+function Entete(props) {
+  return (
+    <Container>
+      <Menu/>
+      <h1>Coup D'Oeil</h1>
+    </Container>
+  )
+}
+
+function Menu(props) {
+  return (
+    <Navbar collapseOnSelect expand="md" bg="info" variant="dark" fixed="top">
+      <Navbar.Brand href='/'>Retour</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-menu" />
+      <Navbar.Collapse id="responsive-navbar-menu">
+        <Nav className="mr-auto" activeKey={props.section}>
+          <p>Menu Elements</p>
+        </Nav>
+        <Nav className="justify-content-end">
+          <Nav.Link eventKey={props.languageChangement} onSelect={props.changeLanguage}><Trans>menu.changerLangue</Trans></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+function Contenu(props) {
+  return (
+    <Container>
+      {props.page}
+    </Container>
+  )
+}
+
+function Footer(props) {
+  return (
+    <Container fluid className="footer bg-info">
+      <Row>
+        <Col>
+          <Row><h2>Information</h2></Row>
+        </Col>
+      </Row>
+      <Row className="millegrille-footer">
+        <Col>
+          <Trans>application.coupdoeilAdvert</Trans>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
