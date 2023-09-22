7a5ccf59e78e952caea61db3e468581b9566bb02
mathieu <mathieu.dugre@mdugre.info>
2023-09-22 15:17:57 -0400

    WIP wiring webapp
    Routes /info.json, /initSession

--- server/WebServerCoupdoeil.py
+++ server/WebServerCoupdoeil.py
@@ -2,13 +2,19 @@ import logging
 
 from millegrilles_web.WebServer import WebServer
 
+from server import Constantes as ConstantesCoupdoeil
+
 
 class WebServerCoupdoeil(WebServer):
 
     def __init__(self, etat, commandes):
         self.__logger = logging.getLogger(__name__ + '.' + self.__class__.__name__)
-        super().__init__(etat, commandes)
+        super().__init__(ConstantesCoupdoeil.WEBAPP_PATH, etat, commandes)
+
+    def get_nom_app(self) -> str:
+        return ConstantesCoupdoeil.APP_NAME
 
     def _preparer_routes(self):
         self.__logger.info("Preparer routes WebServerCoupdoeil sous /coupdoeil")
+        super()._preparer_routes()
         pass
