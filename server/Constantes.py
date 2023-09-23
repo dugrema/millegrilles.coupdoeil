afb3f8745c77c426b66976df385697f982688faf
mathieu <mathieu.dugre@mdugre.info>
2023-09-23 14:15:21 -0400

    Ajout base pour requetes/commandes

--- server/Constantes.py
+++ server/Constantes.py
@@ -2,3 +2,5 @@
 APP_NAME = 'coupdoeil'
 
 WEBAPP_PATH = '/coupdoeil'
+
+EXCHANGE_DEFAUT = '3.protege'
--- server/SocketIoCoupdoeilHandler.py
+++ server/SocketIoCoupdoeilHandler.py
@@ -3,6 +3,10 @@ import asyncio
 from millegrilles_web.EtatWeb import EtatWeb
 from millegrilles_web.SocketIoHandler import SocketIoHandler
 
+from millegrilles_web import Constantes as ConstantesWeb
+
+from server import Constantes as ConstantesCoupdoeil
+
 
 class SocketIoCoupdoeilHandler(SocketIoHandler):
 
@@ -11,3 +15,11 @@ class SocketIoCoupdoeilHandler(SocketIoHandler):
 
     async def _preparer_socketio_events(self):
         await super()._preparer_socketio_events()
+        self._sio.on('coupdoeil/requeteListeNoeuds', handler=self.requete_liste_noeuds)
+
+    @property
+    def exchange_default(self):
+        return ConstantesCoupdoeil.EXCHANGE_DEFAUT
+
+    async def requete_liste_noeuds(self, sid: str, params: dict):
+        return await self.executer_requete(sid, params)
